6e594976322cd4d39d1e811cee230388a11c57c4
scavomigty <157555028+scavomigty@users.noreply.github.com>
2024-05-26 08:04:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let menuItems = document.getElementById("navItems");
+
+// menuItems.style.display="none";
+
+
+menuItems.style.maxHeight = "0px"
+  // )
+ function menutoggle(){
+ 
+ 
+  
+    // alert("hello")
+ if
+      (menuItems.style.maxHeight == "0px")
+  // )
+   {menuItems.style.maxHeight = "200px";}
+  else{menuItems.style.maxHeight = "0px";}
+  
+  // menuItems.style.display ="none";
+
+  // function menutoggle(){
+
+    // if( menuItems.style.display =="none")
+    // { menuItems.style.display ="block";}
+    //  else{ menuItems.style.display ="none";}
+  }
+  
+  
